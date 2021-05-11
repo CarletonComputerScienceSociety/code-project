14229dcd29cfea30880942d648dc02faaea60a09
Matthew M-B <matthewmacraebovell@outlook.com>
2021-05-11 18:23:45 -0400

    setup router (#10)

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-	import { Navbar } from './components'
-	import { Home } from './pages'
+	import Router from 'svelte-spa-router'
+	// import { Navbar } from './components'
+	import { Home, Question, NotFound } from './pages'
+
+	const routes = {
+	  '/': Home,
+	  '/question': Question,
+	  '*': NotFound
+	}
 </script>
 
-<Navbar/>
-<Home/>
+<!--<Navbar/>-->
+<Router {routes}/>
