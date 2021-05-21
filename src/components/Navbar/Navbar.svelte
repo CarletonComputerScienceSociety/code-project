fb7509c99ce4815a5275f3de8a35bea298e8cfe7
Alex <amulvey9966@outlook.com>
2021-05-21 17:22:00 -0400

    added navbar

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -1,4 +1,13 @@
-<div class="navbar"></div>
+<script>
+    import {link} from 'svelte-spa-router';
+</script>
+
+<div class="navbar">
+    <a href="/" use:link class="navbar-item">Home</a>
+    <a href="/questions" use:link class="navbar-item">Questions</a>
+    <a href="/about" use:link class="navbar-item">About</a>
+    <a href="/volunteer" use:link class="navbar-item">Volunteer</a>
+</div>
 
 <style>
     .navbar{
@@ -8,5 +17,22 @@
         box-shadow: 0 2px 4px 1px rgb(0 0 0 / 20%);
         z-index: 10;
         background-color: #4285f4ff;  
+        display: flex;
+        color:white;
+        align-items: center;
+        justify-content: space-evenly;
+        font-weight: 500;
     }
+    
+    a {
+        color: white;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color:white; 
+        text-decoration:none; 
+        cursor:pointer;
+    }
+
 </style>
