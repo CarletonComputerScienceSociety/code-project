178a4ab3c1366763957dc29645358ae3c75b9b8c
MathyouMB <matthewmacraebovell@bell.net>
2021-05-22 12:26:25 -0400

    added top 0 to navbar

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -12,26 +12,27 @@
 <style>
     .navbar {
         position: fixed;
+        top: 0;
         width: 100%;
         min-height: 4rem;
         box-shadow: 0 2px 4px 1px rgb(0 0 0 / 20%);
         z-index: 10;
-        background-color: #4285f4ff;  
+        background-color: #4285f4ff;
         display: flex;
         color:white;
         align-items: center;
         justify-content: space-evenly;
         font-weight: 500;
     }
-    
+
     a {
         color: white;
         text-decoration: none;
     }
 
     a:hover {
-        color:white; 
-        text-decoration:none; 
+        color:white;
+        text-decoration:none;
         cursor:pointer;
     }
 
