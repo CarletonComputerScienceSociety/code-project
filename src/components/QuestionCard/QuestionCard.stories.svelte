ddaa929dcfcff357bf35c32cc97667b158c6260f
Elias Hawa <58529029+EliasJRH@users.noreply.github.com>
2021-05-26 14:35:54 -0400

    12/question card component (#17)
    * Create svelte component for question card
    
    * Add storybook for component
    
    * Remove unecessary imports
    
    * Made storybook customizable
    
    * Change width, height and padding to be in rem units
    
    * Add export for question card in index.ts

---
+++ src/components/QuestionCard/QuestionCard.stories.svelte
@@ -0,0 +1,48 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+    import QuestionCard from './QuestionCard.svelte'
+</script>
+
+
+<Meta
+    title="Example/QuestionCard"
+    component={QuestionCard}
+    argTypes={{
+        level: {
+            control: { type: 'number', min: 1, max: 3, step: 1 }
+        },
+        title: { control: 'text' },
+        description: { control: 'text' }
+    }}
+/>
+
+<Template let:args>
+    <QuestionCard {...args}/>
+</Template>
+
+<Story 
+    name="Basic"
+    args={{
+        level: 1,
+        title: 'P1: Hello World',
+        description: 'Print hello world to console'
+    }}
+/>
+
+<Story 
+    name="Intermediate"
+    args={{
+        level: 2,
+        title: 'P2: Simple Graphics Drawing',
+        description: 'Draw a 1:1 recreation of Da Vinci\'s Mona Lisa with Simple Graphics'
+    }}
+/>
+
+<Story 
+    name="Challenging"
+    args={{
+        level: 3,
+        title: 'P3: Make google',
+        description: 'Refer to the title'
+    }}
+/>
---
+++ src/components/QuestionCard/QuestionCard.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    export let level: number
+    export let title: string
+    export let description: string
+</script>
+
+<div class="question_button">
+    <div class="question_content">
+        <span style="line-height: 0px">
+            {#if level === 1}
+            <h5 style="color: #28a74c">Basic</h5>
+            {:else if level === 2}
+            <h5 style="color: #0080ff">Intermediate</h5>
+            {:else if level === 3}
+            <h5 style="color: #f54425">Challenging</h5>
+            {/if}
+            <h3>{title}</h3>
+        </span>
+        <span class="truncated_text">{description}</span>
+    </div>
+  </div>
+
+<style>
+    .question_button{
+        font-family: 'Roboto', sans-serif; 
+        justify-content: center;
+        height: 14.375rem;
+        width: 21.875rem;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+        border-radius: 1.5%;
+    }
+
+    .question_content{
+        padding: 0.938rem 1.25rem 1.25rem 1.25rem;
+    }
+
+    .truncated_text{
+        display: -webkit-box;
+        -webkit-line-clamp: 7;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+</style>
