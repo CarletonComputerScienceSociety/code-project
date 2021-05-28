dfd0e35a3d9036997ce4d9ee8bc3f6ab789fff42
AdiChops <Chopra.Aaditya@cegep-heritage.qc.ca>
2021-05-27 22:35:40 -0400

    test removal

--- src/components/Input/__tests__/Input.test.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/svelte'
-import Input from '../Input.svelte'
-
-test('QuestionAnswer succesfully renders', () => {
-  const results = render(Input, { props: { label: 'Test' } })
-
-  expect(() => results.getByText('Test')).not.toThrow()
-})
