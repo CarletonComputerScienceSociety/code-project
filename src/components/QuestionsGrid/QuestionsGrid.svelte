ea22d31d4d6d94d8faa02c8d56c8750a21e4f500
Elias Hawa <elias.hawa2468@gmail.com>
2021-06-01 11:17:19 -0400

    Add condition if questions list is empty

--- src/components/QuestionsGrid/QuestionsGrid.svelte
+++ src/components/QuestionsGrid/QuestionsGrid.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
     import { QuestionCard } from '../../components'
     import { link } from 'svelte-spa-router'
+import { mount_component } from 'svelte/internal'
 
     export let questions = []
 </script>
 
 
 <div class="questions-grid">
-    {#each questions as question}
-        <a href={'/question/' + question.id} use:link>
-            <QuestionCard level={question.level} title={question.title} description={question.description}/>
-        </a>
-    {/each}
+    {#if questions.length === 0}
+        <h1>No questions here at the moment. Check back soon!</h1>
+    {:else}
+        {#each questions as question}
+            <a href={'/question/' + question.id} use:link>
+                <QuestionCard level={question.level} title={question.title} description={question.description}/>
+            </a>
+        {/each}
+    {/if}
 </div>
 
 <style>
